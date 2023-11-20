7f317d58dc6a2e6ffd4825891a6a4ebae0f66431
melmuller <melmullermoraes@gmail.com>
2023-11-20 08:55:40 -0300

    estrutura do projeto

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const router = require('./routes/rotas');
+require('dotenv').config();
+
+
+app.set('port', process.env.PORT || 3333);
+app.use('/api', router);
+
+module.exports = app;
